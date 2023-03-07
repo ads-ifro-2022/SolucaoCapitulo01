87acdbc5db567a9eb36f95697ab36249b2c922d4
Aluno <Aluno@LABINFO-9DC9A4>
2023-03-07 19:16:16 -0400

    Adicionar arquivos de projeto.

---
+++ TrocaDeValores/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TrocaDeValores
+{
+    partial class frmTrocaDeValores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(13, 27);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(40, 13);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Valor 1";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(59, 24);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(39, 20);
+            this.txtValor1.TabIndex = 1;
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(114, 27);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(40, 13);
+            this.lblValor2.TabIndex = 2;
+            this.lblValor2.Text = "Valor 2";
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(160, 24);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(42, 20);
+            this.txtValor2.TabIndex = 3;
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.Location = new System.Drawing.Point(219, 23);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(75, 23);
+            this.btnTrocar.TabIndex = 4;
+            this.btnTrocar.Text = "Trocar";
+            this.btnTrocar.UseVisualStyleBackColor = true;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // frmTrocaDeValores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 76);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor1);
+            this.Name = "frmTrocaDeValores";
+            this.Text = "Troca de valores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnTrocar;
+    }
+}
+
---
+++ TrocaDeValores/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaDeValores
+{
+    public partial class frmTrocaDeValores : Form
+    {
+        public frmTrocaDeValores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            string auxiliar;
+            auxiliar = txtValor1.Text;
+            txtValor1.Text = txtValor2.Text;
+            txtValor2.Text = auxiliar;
+
+            MessageBox.Show("Troca de valores concluída", "Informação",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
